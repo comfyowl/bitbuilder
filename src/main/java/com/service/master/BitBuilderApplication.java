187332580d2511a1d66154fc171416ae8dca775f
Ovidiu Rosu <ovidiu.rosu@lumafintech.com>
2025-08-05 16:28:20 +0300

    Commit

--- src/main/java/com/service/master/BitBuilderApplication.java
+++ src/main/java/com/service/master/BitBuilderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MasterApplication {
+public class BitBuilderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MasterApplication.class, args);
+		SpringApplication.run(BitBuilderApplication.class, args);
 	}
 
 }
